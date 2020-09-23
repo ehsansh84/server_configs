337a97975a2454aee52b1c8c5172e04506c195f2
Ehsan Shirzadi <ehsan.shirzadi@gmail.com>
2020-09-23 19:30:12 +0330

    Create init_server.sh

---
+++ init_server.sh
@@ -0,0 +1,25 @@
+apt update
+apt install -y vim
+apt install -y screenfetch
+apt install -y curl
+apt install -y git
+apt install -y htop
+apt install -y iotop
+apt install -y nload
+apt install -y net-tools
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -    
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+apt install docker-compose
