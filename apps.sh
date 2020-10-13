ea10d890a0c96cfafb86168c318bac475c5da3f1
Ehsan Shirzadi <ehsan.shirzadi@gmail.com>
2020-10-13 17:40:17 +0330

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,5 +1,8 @@
+mkdir -p softwares
+cd softwares
 wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
-apt-get install -y ./nordvpn-release_1.0.0_all.deb
+apt install ./nordvpn-release_1.0.0_all.deb
 apt-get update
-apt-get install -y nordvpn
+apt-get install nordvpn
 wget https://download-cf.jetbrains.com/python/pycharm-community-2020.2.3.tar.gz
+tar xvfz pycharm-community-2020.2.3.tar.gz
