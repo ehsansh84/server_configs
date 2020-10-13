cb8c746982a5e8a7f363b0a1bea19a8b58a331ad
Ehsan Shirzadi <ehsan.shirzadi@gmail.com>
2020-10-13 17:24:08 +0330

    Create apps.sh

---
+++ apps.sh
@@ -0,0 +1,5 @@
+wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
+apt-get install -y ./nordvpn-release_1.0.0_all.deb
+apt-get update
+apt-get install -y nordvpn
+wget https://download-cf.jetbrains.com/python/pycharm-community-2020.2.3.tar.gz
