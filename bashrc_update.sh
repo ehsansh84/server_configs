92cce1e72c66bba311caa7b242c784c51cf90b7b
Ehsan Shirzadi <ehsan.shirzadi@gmail.com>
2020-10-17 16:28:55 +0330

    Create bashrc_update.sh

---
+++ bashrc_update.sh
@@ -0,0 +1,8 @@
+echo 'alias s150="ssh debian@45.93.170.150"' >> .bashrc
+echo 'alias s68="ssh ehsan@5.63.9.68"' >> .bashrc
+echo 'alias smiz="ssh root@server1.onmiz.org"' >> .bashrc
+echo 'alias sg="ssh root@148.251.17.194"' >> .bashrc
+echo 'alias nord="nordvpn connect London"' >> .bashrc
+echo 'alias nordusa="nordvpn connect Dallas"' >> .bashrc
+echo 'alias dnord="nordvpn disconnect"' >> .bashrc
+source .bashrc
