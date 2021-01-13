fcc2647394d64e10d8c6a8c2bc2129d86f052a7f
Ehsan Shirzadi <ehsan.shirzadi@gmail.com>
2021-01-13 14:00:40 +0330

    Update bashrc_update.sh

--- bashrc_update.sh
+++ bashrc_update.sh
@@ -1,5 +1,6 @@
 echo 'alias s150="ssh debian@45.93.170.150"' >> .bashrc
 echo 'alias s68="ssh ehsan@5.63.9.68"' >> .bashrc
+echo 'alias s146="ssh root@136.243.249.146"' >> .bashrc
 echo 'alias smiz="ssh root@server1.onmiz.org"' >> .bashrc
 echo 'alias sg="ssh root@148.251.17.194"' >> .bashrc
 echo 'alias nord="nordvpn connect London"' >> .bashrc
