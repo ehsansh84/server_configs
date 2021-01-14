dfa674cac962ca0c3aca115f4b3da5e426e5f22e
Ehsan Shirzadi <ehsan.shirzadi@gmail.com>
2021-01-14 16:02:10 +0330

    Update bashrc_update.sh

--- bashrc_update.sh
+++ bashrc_update.sh
@@ -6,4 +6,5 @@ echo 'alias sg="ssh root@148.251.17.194"' >> .bashrc
 echo 'alias nord="nordvpn connect London"' >> .bashrc
 echo 'alias nordusa="nordvpn connect Dallas"' >> .bashrc
 echo 'alias dnord="nordvpn disconnect"' >> .bashrc
+echo 'export PS1="\[\e[32m\][\[\e[m\]\[\e[31m\]\u\[\e[m\]\[\e[33m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[36m\]\w\[\e[m\]\[\e[32m\]]\[\e[m\]\[\e[32;47m\]\\$\[\e[m\] "'
 source .bashrc
